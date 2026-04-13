043db7622c015a55ae9b5728a06664e7e9734cd2
bielpieroli <joaogabrielpieroli18@gmail.com>
2026-04-13 17:30:44 -0300

    fix: phonenumber

--- front/src/constants/DonnaLupeInfo.ts
+++ front/src/constants/DonnaLupeInfo.ts
@@ -2,7 +2,7 @@ export const BrandInfo = {
     "LOCATION": "ICMC-USP, São Carlos - SP",
     "EMAIL": "ludogs_ufscar@gmail.com",
     "INSTAGRAM": "https://www.instagram.com/ludogs_br/",
-    "TELEFONE": "+55 11 95984-5951",
+    "TELEFONE": "+55 16 99182-8804",
 }
 
 export default BrandInfo;
