25a045d386908c98f05034460fa872f4c1737e0f
bielpieroli <joaogabrielpieroli18@gmail.com>
2026-05-18 10:01:36 -0300

    feat: first backend version

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar .env")
+	}
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	port := os.Getenv("PORT")
+	r.Run(":" + port)
+}
